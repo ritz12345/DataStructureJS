f21f232b0f96f11b236a30720a1c4ce57c3bc20d
Ritaban Chowdhury <uni2k11@gmail.com>
2018-10-07 13:56:21 -0400

    changed everything properly

--- LinkedList.js
+++ LinkedList.js
@@ -1,13 +1,18 @@
 "use strict";
 //Build a constructor function for Linked Lists
-function LinkedList(head,tail){
-	this.head= head;
-	this.tail= tail;
+function LinkedList(){
+	this.head= null;
+	this.tail= null;
 }
 
 
 
 //Build a Constucto function for Nodes.
-function Node(){
-
+function Node(value,next,previous){
+	this.value=value;
+	this.next =next;
+	this.previous=previous;
+}
+LinkedList.prototype.addToHead=function(value){
+	var newNode =new Node(value,this.head,null);
 }
